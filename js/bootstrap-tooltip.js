6ed7f9b8f90c53a371427d7055ad7128af10e8b9
Jason Ramos <jason@stackideas.com>
2012-11-28 13:41:56 +0800

    [bootstrap] Use modifed tooltip with container option from Yohn.

--- js/bootstrap-tooltip.js
+++ js/bootstrap-tooltip.js
@@ -1,5 +1,5 @@
 /* ===========================================================
- * bootstrap-tooltip.js v2.2.1
+ * bootstrap-tooltip.js v2.2.2
  * http://twitter.github.com/bootstrap/javascript.html#tooltips
  * Inspired by the original jQuery.tipsy by Jason Frame
  * ===========================================================
@@ -121,8 +121,22 @@
         $tip
           .detach()
           .css({ top: 0, left: 0, display: 'block' })
-          .insertAfter(this.$element)
 
+        switch (this.options.container || 'parent') {
+          case "parent":
+            $tip.insertAfter(this.$element);
+          break;
+          case "body":
+            $tip.appendTo(document.body);
+          break;
+          default:
+            var container = $(this.options.container);
+            if (container.length) {
+              $tip.appendTo(container);
+            } else {
+              $tip.insertAfter(this.$element);
+            }
+        }
         pos = this.getPosition(inside)
 
         actualWidth = $tip[0].offsetWidth
@@ -271,6 +285,7 @@
   , title: ''
   , delay: 0
   , html: false
+  , container: 'parent'
   }
 
 }($);
